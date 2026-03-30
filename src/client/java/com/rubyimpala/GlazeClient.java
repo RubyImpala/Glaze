de33782e2b9397c8c4929e7202860c8f40128793
RubyImpala <wars.co.il@gmail.com>
2026-03-31 00:12:32 +0300

    Initial commit

---
+++ src/client/java/com/rubyimpala/GlazeClient.java
@@ -0,0 +1,10 @@
+package com.rubyimpala;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GlazeClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
